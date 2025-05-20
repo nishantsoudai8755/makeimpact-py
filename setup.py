530fecdde0c39bea743b98f5a17ffec09d2b4c58
Chirag Sachdeva <chiragsachdeva100@gmail.com>
2025-05-20 12:49:25 -0400

    Update version number to 1.0.0 in setup.py and add PUBLISHING.md to .gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="makeimpact",
-    version="0.1.0",
+    version="1.0.0",
     author="1ClickImpact",
     author_email="info@1clickimpact.com",
     description="Python SDK for 1ClickImpact API",
