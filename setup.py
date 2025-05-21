30803b13c87c22506ecb00c8d4fc863db3410e14
Chirag Sachdeva <chiragsachdeva100@gmail.com>
2025-05-21 13:11:16 -0400

    Bump version to 1.1.1 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="makeimpact",
-    version="1.0.1",
+    version="1.1.1",
     author="1ClickImpact",
     author_email="contact@1clickimpact.com",
     description="Python SDK for 1ClickImpact API",
